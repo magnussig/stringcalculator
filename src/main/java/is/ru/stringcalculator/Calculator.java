d46551eebf48325f11076c169029a0e2d90236fc
Magnús Sigurðarson <maggisig92@gmail.com>
2014-10-18 21:30:02 +0000

    new line accepted

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,32 +2,45 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
-		if(text.equals("")){
+	public static int add(String text)
+	{
+		if(text.equals(""))
+		{
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",") || text.contains("\n"))
+		{
 			return sum(splitNumbers(text));
 		}
+		/*else if(text.contains("\n"))
+		{
+			return sum(splitOnNewline(text));
+		}*/
 		else
+		{
 			return 1;
+		}
 	}
 
-	private static int toInt(String number){
+	private static int toInt(String number)
+	{
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	private static String[] splitNumbers(String numbers)
+	{
+		return numbers.split(",|\\n"); 	
 	}
       
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
+	private static int sum(String[] numbers)
+	{
+ 		int total = 0;
+        	for(String number : numbers)
+		{	
+	   		total += toInt(number);
 		}
 		return total;
-    }
+    	}
 
 
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,28 +5,38 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
-    }
+	public static void main(String args[]) 
+	{
+      		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    	}
 
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() 
+	{
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber() 
+	{
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
-	public void testTwoNumbers() {
+	public void testTwoNumbers() 
+	{
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
+    	public void testMultipleNumbers()
+	{
+    		assertEquals(6, Calculator.add("1,2,3"));
+    	}
+	@Test
+	public void testSplitOnNewline()
+	{
+		assertEquals(6, Calculator.add("1\n2,3"));
+		assertEquals(3, Calculator.add("1\n2"));
+	}
 }
